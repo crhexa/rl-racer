df529abcd4046c6b34d6d787b8bf142143d7530f
corrupthexagons <d4rkM4g3r@gmail.com>
2024-02-15 14:38:16 -0500

    Added the placing of additional cars

--- game/Track.cs
+++ game/Track.cs
@@ -1,30 +1,42 @@
 using Godot;
 using System;
-using System.Threading;
 
 public partial class Track : Node
 {
+	[Export] public bool agentControl = true;
 	[Export] public int humanControl = 0;
 	[Export] public int numberCars = 1;
+	[Export] public float horizontalOffset = 280f;
+	[Export] public float verticalOffset = 480f;
 
 	private PackedScene carScene;
-	private Car[] cars = new Car[1];
+	private Car[] cars;
 	private TrackCurve track;
 	private Node sync;
 
 	public override void _Ready()
 	{
+		cars = new Car[numberCars];
 		carScene = GD.Load<PackedScene>("res://game/car.tscn");
 
 		sync = GetNode<Node>("Sync");
 		track = GetNode<TrackCurve>("TrackCurve");
 		cars[0] = GetNode<Car>("Car_0");
 
+		Vector2 start = cars[0].Position;
+		cars[0].startingPosition = start;
+		for (int i = 1; i < numberCars; i++) {
+			cars[i] = carScene.Instantiate<Car>();
+			PlaceCar(cars[i], start, i);
+			AddChild(cars[i]);
+		}
+		MoveChild(sync, -1);
+
 		foreach (Car car in cars) {
 			car.SetTrack(track);
 		}
 
-		if (humanControl > -1 && numberCars < 2) {
+		if (!agentControl || (humanControl > -1 && numberCars < 2)) {
 			sync.Set("disabled", true);
 		}
 	}
@@ -53,4 +65,13 @@ public partial class Track : Node
 		}
 	}
 
+	private void PlaceCar(Car car, Vector2 start, float i) {
+		if (i % 2 == 0) {
+			car.startingPosition = new(start.X, start.Y + (0.5f * i * verticalOffset));
+
+		} else {
+			car.startingPosition = new(start.X + horizontalOffset, start.Y + (0.5f * i * verticalOffset));
+		}
+		car.Position = car.startingPosition;
+	}
 }
